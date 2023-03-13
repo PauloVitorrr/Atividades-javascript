d85345e3aac8937df1b32c98c15287e9efa81184
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-13 19:47:43 -0300

    exercicio 02

---
+++ Exercícios - curso fundamentos em JS/exercicios js/exercicios/ex02.js
@@ -0,0 +1,8 @@
+//Escreva um função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias
+//OBS: desconsiderar anos bissextos e
+
+function converterIdadeEmAnosParaDias(anos){
+    return console.log(anos * 365)
+}
+
+converterIdadeEmAnosParaDias(25)
