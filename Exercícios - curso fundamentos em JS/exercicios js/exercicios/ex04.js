471553dfc00b26674d595dd4d43e7a735b5bfb47
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-13 21:04:44 -0300

    Atividade 4

---
+++ Exercícios - curso fundamentos em JS/exercicios js/exercicios/ex04.js
@@ -0,0 +1,44 @@
+//crie uma função que recebe um número (de 1 a 12) e retorne o mes correspondente como uma string.
+//por exemplo se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mes;
+function nomeDoMes(mes){
+    switch (mes){
+        case 1:
+            console.log("Janeiro")
+        break;
+        case 2:
+            console.log("Fevereiro")
+        break;
+        case 3:
+            console.log("Março")
+        break;
+        case 4:
+            console.log("Abril")
+        break;
+        case 5:
+            console.log("Maio")
+        break;
+        case 6:
+            console.log("Junho")
+        break;
+        case 7:
+            console.log("Julho")
+        break;
+        case 8:
+            console.log("Agosto")
+        break;
+        case 9:
+            console.log("Setembro")
+        break;
+        case 10:
+            console.log("Outubro")
+        break;
+        case 11:
+            console.log("Novembro")
+        break;
+        case 12:
+            console.log("Dezembro")
+        break;
+    }
+}
+
+nomeDoMes(12)
