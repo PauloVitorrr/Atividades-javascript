11b76d0f3d7cbac256f6cd7866ebd9944c459142
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-14 22:59:25 -0300

    exercicio 03

---
+++ Exercícios - fundamentos 1/exercicio/ex03.js
@@ -0,0 +1,6 @@
+//Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+function baseExpoente(base, expoente){
+    return console.log(`${Math.pow(base,expoente)}`)
+}
+
+baseExpoente(3,3)
