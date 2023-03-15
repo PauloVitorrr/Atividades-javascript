1da9cad3c14914bb5dae1c020b78b8db0143956d
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-14 22:34:58 -0300

    exercicio 1

---
+++ Exercícios - fundamentos 1/exercicio/ex01.js
@@ -0,0 +1,11 @@
+//Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+//multiplicação e divisão desses valores
+
+function contas(n1,n2){
+    console.log(`A soma dos dois numeros é ${n1 + n2}`)
+    console.log(`A subtração dos dois numeros é ${n1 - n2}`)
+    console.log(`A divisão dos dois números é ${n1 / n2}`)
+    console.log(`A multiplicação dos dois numéros é ${n1 * n2}`)
+}
+
+contas(10,5)
