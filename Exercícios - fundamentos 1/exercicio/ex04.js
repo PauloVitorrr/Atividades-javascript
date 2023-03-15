3823c375d8869c682ea8c4379967691ea941a214
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-14 22:59:31 -0300

    Exercicio 4

---
+++ Exercícios - fundamentos 1/exercicio/ex04.js
@@ -0,0 +1,10 @@
+//Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+//e o resto da divisão destes dois valores.
+function divisor(n1,n2){
+    const divisao = n1 / n2
+    console.log(divisao)
+    const restoDivisao = n1 % n2
+    console.log(restoDivisao)
+}
+
+divisor(7 , 3)
