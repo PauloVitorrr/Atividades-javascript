993b65c6f7803a54cbf6d1922b340025285235e6
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-22 17:47:20 -0300

    Atividade 12

---
+++ Exercícios - fundamentos 1/exercicio/ex12.js
@@ -0,0 +1,10 @@
+// Faça um algoritmo que calcule o fatorial de um número.
+function factorialize(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+console.log(factorialize(5))
