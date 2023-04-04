89a224cdea81d67283bc63e5ab2d5ce439eeee78
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-04-03 21:37:31 -0300

    Atividade 26

--- Exercícios - fundamentos 1/exercicio/ex26.js
+++ Exercícios - fundamentos 1/exercicio/ex26.js
@@ -1,9 +1,9 @@
 // Crie um construtor de função chamado Pessoa que tenha propriedades para nome, idade e profissão. Em seguida, crie uma instância do objeto Pessoa com o nome "João", idade "30" e profissão "engenheiro".
-function Pessoa(nome, idade,profissao){
+function pessoa(nome, idade,profissao){
     this.nome = nome
     this.idade = idade
     this.profissao = profissao
 }
 
-const joao = new Pessoa("João", 30, "engenheiro")
+const joao = new pessoa("João", 30, "engenheiro")
 console.log(joao)
