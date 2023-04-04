d30db28d0432b2ffede962d63d39b5796456b2a1
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-04-03 21:38:11 -0300

    Atividade 27

--- Exercícios - fundamentos 1/exercicio/ex27.js
+++ Exercícios - fundamentos 1/exercicio/ex27.js
@@ -1,9 +1,9 @@
 // Crie um construtor de função chamado Retângulo que tenha propriedades para largura e altura. Em seguida, crie um método no protótipo do objeto Retângulo que calcule a área do retângulo. Por fim, crie uma instância do objeto Retângulo com largura 5 e altura 10 e chame o método area().
-function retangulo(largura, altura){
+function Retangulo(largura, altura){
     this.largura = largura
     this.altura = altura
     this.areatotal = altura * largura
 }
 
-const area = new retangulo(5, 10)
+const area = new Retangulo(5, 10)
 console.log(area)
